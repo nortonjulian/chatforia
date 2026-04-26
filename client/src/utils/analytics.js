f58c4e7708619ae2995e5b5ab0aa303029b94625
Julian Norton <nortonjulian@gmail.com>
2026-04-25 18:09:16 -0600

    all

--- client/src/utils/analytics.js
+++ client/src/utils/analytics.js
@@ -1,9 +1,19 @@
 import posthog from 'posthog-js';
 
-posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
-  api_host: 'https://app.posthog.com',
-  autocapture: true,
-  capture_pageview: true,
-});
+const key = import.meta.env.VITE_POSTHOG_KEY;
+
+console.log('[PostHog] key exists?', Boolean(key));
+
+if (key) {
+  posthog.init(key, {
+    api_host: 'https://us.i.posthog.com',
+    autocapture: true,
+    capture_pageview: true,
+    loaded: (ph) => {
+      console.log('[PostHog] loaded');
+      ph.capture('test_event');
+    },
+  });
+}
 
 export default posthog;
