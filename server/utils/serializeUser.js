b77b3631334c3ea2b143d48044c968fe8c495db4
Julian Norton <nortonjulian@gmail.com>
2026-06-05 19:46:24 -0600

    all

--- server/utils/serializeUser.js
+++ server/utils/serializeUser.js
@@ -18,9 +18,42 @@ export function serializeUser(user) {
     role: user.role ?? 'USER',
     isPremium: premium,
 
+    preferredLanguage: user.preferredLanguage ?? 'en',
+    uiLanguage: user.uiLanguage ?? user.preferredLanguage ?? 'en',
+
     theme:
       premium || FREE_THEMES.includes(user.theme)
         ? user.theme ?? 'dawn'
         : 'dawn',
+
+    avatarUrl: user.avatarUrl ?? null,
+
+    autoTranslate: user.autoTranslate ?? true,
+    showOriginalWithTranslation: user.showOriginalWithTranslation ?? true,
+    allowExplicitContent: user.allowExplicitContent ?? false,
+    showReadReceipts: user.showReadReceipts ?? true,
+    autoDeleteSeconds: user.autoDeleteSeconds ?? 0,
+
+    privacyBlurEnabled: user.privacyBlurEnabled ?? false,
+    privacyBlurOnUnfocus: user.privacyBlurOnUnfocus ?? false,
+    privacyHoldToReveal: user.privacyHoldToReveal ?? false,
+    notifyOnCopy: user.notifyOnCopy ?? false,
+
+    ageBand: user.ageBand ?? null,
+    wantsAgeFilter: user.wantsAgeFilter ?? true,
+    randomChatAllowedBands: user.randomChatAllowedBands ?? [],
+    foriaRemember: user.foriaRemember ?? true,
+
+    voicemailEnabled: user.voicemailEnabled ?? true,
+    voicemailAutoDeleteDays: user.voicemailAutoDeleteDays ?? null,
+    voicemailForwardEmail: user.voicemailForwardEmail ?? null,
+    voicemailGreetingText: user.voicemailGreetingText ?? null,
+    voicemailGreetingUrl: user.voicemailGreetingUrl ?? null,
+
+    messageTone: user.messageTone ?? 'Default.mp3',
+    ringtone: user.ringtone ?? 'Classic.mp3',
+    soundVolume: user.soundVolume ?? 70,
+
+    enableSmartReplies: user.enableSmartReplies ?? false,
   };
 }
